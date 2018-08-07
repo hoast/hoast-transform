181682883e87cc13205a83735cce4e02bf64fbdf
Ron Dekker <redkenrok@gmail.com>
2018-08-07 17:28:07 +0200

    Fixed options issue, updated peer dependencies

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ const getTransformer = function(extension) {
 };
 
 const validateOptions = function(options) {
+	if (!options) {
+		return; // Since no option is required.
+	}
+	
 	assert(typeof(options) === 'object', 'hoast-transform: options must be of type object.');
 	if (options.options) {
 		assert(typeof(options.options) === 'object', 'hoast-transform: options must be of type object.');
